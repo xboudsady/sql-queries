ee3ebd4603fa14e6d2db08a6e81025afc153ca55
xboudsady <boudsadyx@gmail.com>
2019-08-15 00:44:46 +0000

    Added insert records, and added a simple select

---
+++ insert/insert-record.sql
@@ -0,0 +1,15 @@
+-- Use the following command to insert a record into the table.
+-- 1. Specify the table, assuming you already in the database
+-- 2. Specify the columns,
+-- 3. The values of each columns
+
+-- NOTE: The orders matter.
+
+INSERT INTO table_name
+            (column_1,
+             column_2)
+VALUES      ('value_1', 
+             'value_2');
+             
+
+-- To view records in table, use the SELECT * FROM table_name;
---
+++ selects/select.sql
@@ -0,0 +1,5 @@
+-- Use the following command to view records, or query records
+
+-- View all records from a TABLE;
+
+SELECT * FROM table_name;
