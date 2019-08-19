77919c1c5207534741d0b942f06887e95e0edabb
xboudsady <boudsadyx@gmail.com>
2019-08-19 00:19:01 +0000

    Updated delete sql queries command.

--- delete/delete-record.sql
+++ delete/delete-record.sql
@@ -17,6 +17,7 @@
 
 
 
+-- DELTE all cats with the name 'Egg'
 
 DELETE FROM cats WHERE name = 'Egg';
 
@@ -31,3 +32,47 @@ DELETE FROM cats WHERE name = 'Egg';
 -- |      7 | Jack           | Sphynx            |    7 |
 -- +--------+----------------+-------------------+------+
 -- 6 rows in set (0.00 sec)
+
+
+
+-- DELETE all 4 year old cats
+
+DELETE FROM cats WHERE age = 4;
+
+-- +--------+----------------+-----------+------+
+-- | cat_id | name           | breed     | age  |
+-- +--------+----------------+-----------+------+
+-- |      2 | Cindy          | Main Coon |   12 |
+-- |      3 | Dumbledore     | Main Coon |   12 |
+-- |      5 | Misty          | Shorthair |   14 |
+-- |      6 | George Michael | Ragdoll   |    9 |
+-- |      7 | Jack           | Sphynx    |    7 |
+-- +--------+----------------+-----------+------+
+-- 5 rows in set (0.00 sec)
+
+
+
+
+-- DELETE cats whose age is the same as their cat_id
+
+DELETE FROM cats where age = cat_id;
+
+-- +--------+----------------+-----------+------+
+-- | cat_id | name           | breed     | age  |
+-- +--------+----------------+-----------+------+
+-- |      2 | Cindy          | Main Coon |   12 |
+-- |      3 | Dumbledore     | Main Coon |   12 |
+-- |      5 | Misty          | Shorthair |   14 |
+-- |      6 | George Michael | Ragdoll   |    9 |
+-- +--------+----------------+-----------+------+
+-- 4 rows in set (0.00 sec)
+
+
+
+
+-- DELETE all cats
+
+DELETE FROM cats;
+
+-- Empty set (0.00 sec)
+
